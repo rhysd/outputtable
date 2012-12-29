9d3420ff74a60f3cb7a9ef2013cc39d70d1d244c
rhysd <lin90162@yahoo.co.jp>
2012-12-30 00:40:04 +0900

    check std::begin() and std::end() are able to be applied

--- include/outputtable/container/detail/containers_to_stream.hpp
+++ include/outputtable/container/detail/containers_to_stream.hpp
@@ -2,10 +2,33 @@
 #define      OUTPUTTABLE_IMPL_CONTAINERS_TO_STREAM_HPP_INCLUDED
 
 #include <iostream>
+#include <type_traits>
 
 namespace output_containers_impl {
+    template<class T>
+    class has_begin{
+        template<class U>
+        static auto check(U v) -> decltype( std::begin(v), std::true_type() );
+        static auto check(...) -> decltype( std::false_type() );
+    public:
+        typedef decltype( check( std::declval<T>() ) ) type;
+        static bool const value = type::value;
+    };
+
+    template<class T>
+    class has_end{
+        template<class U>
+        static auto check(U v) -> decltype( std::end(v), std::true_type() );
+        static auto check(...) -> decltype( std::false_type() );
+    public:
+        typedef decltype( check( std::declval<T>() ) ) type;
+        static bool const value = type::value;
+    };
+
     template<class Container>
-    void container_to_stream(std::ostream &ost, Container const& c)
+    void container_to_stream( std::ostream &ost, Container const& c,
+                              typename std::enable_if< has_begin<Container>::value &&
+                                                       has_end<Container>::value >::type* = 0)
     {
         ost << "{ ";
         for(auto const& i : c){
@@ -13,6 +36,7 @@ namespace output_containers_impl {
         }
         ost << '}';
     }
+
 } // namespace output_containers_impl
 
 #endif    // OUTPUTTABLE_IMPL_CONTAINERS_TO_STREAM_HPP_INCLUDED
